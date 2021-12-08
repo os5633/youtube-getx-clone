8db8cdae6bdc0a00e85dde039823bdd96d16641b
os5633 <71377244+os5633@users.noreply.github.com>
2021-12-08 20:41:32 +0900

    pakage설치 및 route세팅

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/route/route.dart';
+import 'package:get/get.dart';
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
+    return GetMaterialApp(
+      title: "Youtube Clone App",
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      getPages: routes,
     );
   }
 }
