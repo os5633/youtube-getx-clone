402c41781d464d24ca833a1e401b284b3ca0f86c
os5633 <71377244+os5633@users.noreply.github.com>
2021-12-09 23:28:20 +0900

    video리스트 구현

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/src/widget/custom_appbar.dart';
+import 'package:flutter_application_1/src/widget/video.dart';
+import 'package:get/get.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -16,9 +18,11 @@ class Home extends StatelessWidget {
           ),
           SliverList(
             delegate: SliverChildBuilderDelegate((context, index) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(height: 100, color: Colors.grey),
+              return InkWell(
+                onTap: () {
+                  Get.toNamed("/videoview/233");
+                },
+                child: const Video(),
               );
             }, childCount: 100),
           )
