dfa29028a050bda9448a16b2433e9abaa0cf3168
os5633 <71377244+os5633@users.noreply.github.com>
2021-12-11 18:33:35 +0900

    video 재생화면 구현

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/src/widget/custom_appbar.dart';
-import 'package:flutter_application_1/src/widget/video.dart';
+import 'package:flutter_application_1/src/widget/list_video.dart';
 import 'package:get/get.dart';
 
 class Home extends StatelessWidget {
